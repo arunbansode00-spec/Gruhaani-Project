f7375d4c199e67d5c32c4aa03a2caba96f25ec68
Arun Bansode <arunbansode00@gmail.com>
2026-02-22 01:57:44 +0530

    connected production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://gruhaani-backend.onrender.com/api"
 });
 
 // ⭐ attach token automatically
